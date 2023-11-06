40e4dbde7f496281a11adb0ae023c0dc81bba263
Kalobd12 <143021970+Kalobd12@users.noreply.github.com>
2023-11-06 08:08:20 -0500

    using the set and get method to both assign and return the value for each one.

--- petclass.py
+++ petclass.py
@@ -14,3 +14,12 @@ class Pet
     # The set_age method accepts an argument for the pets age.
     def set_age(self, age):
         self.__age = age
+    # The get_name method returns the pets name.   
+    def get_name(self):
+        return self.__name
+    # The get_animal_type method returns the pets animal type.
+    def get_animal_type(self):
+        return self.__animal_type
+    # The get_age method returns the pets age.
+    def get_age(self):
+        return self.__age
