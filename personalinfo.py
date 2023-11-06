388aedf0443bf9c4e9e96de454ae9f9289e840c9
Kalobd12 <143021970+Kalobd12@users.noreply.github.com>
2023-11-06 08:58:46 -0500

    creating class for personalinfo

---
+++ personalinfo.py
@@ -0,0 +1,8 @@
+# PersonalData class holds data about personal information.
+class PersonalData
+    # The __init__ method initializes the attributes.
+    def __init__(self, name, address, age, phone_number):
+        self.__name = name
+        self.__address = address
+        self.__age = age
+        self.__phone_number = phone_number
