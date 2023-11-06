255500875bf2495a46b63cb5fb545ba7714df15a
Kalobd12 <143021970+Kalobd12@users.noreply.github.com>
2023-11-06 07:46:31 -0500

    setting up petclass

---
+++ petclass.py
@@ -0,0 +1,7 @@
+# Pet class holds data about pet information.
+class Pet
+    # The __init__ method initializes the attributes.
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
