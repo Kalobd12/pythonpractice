fe71467b4e1ceecaf8f52a7e5821ed9a9cea996b
Kalobd12 <143021970+Kalobd12@users.noreply.github.com>
2023-11-06 09:20:14 -0500

    setting get method to return the persons personal info.

--- personalinfo.py
+++ personalinfo.py
@@ -16,6 +16,18 @@ class PersonalData:
     def set_age(self, address):
         self.__age = age
     # The set_phone_number method accepts an argument for the persons phone number.
-    def set_phone_number(self, phone_number)
+    def set_phone_number(self, phone_number):
         self.__phone_number = phone_number
+    # The get_name method returns the persons name.   
+    def get_name(self):
+        return self.__name
+    # The get_address method returns the persons address.
+    def get_address(self):
+        return self.__address
+    # The get_age method returns the persons age.
+    def get_age(self):
+        return self.__age
+    # The get_phone_number method returns the persons phone number.
+    def get_phone_number(self):
+        return self.__phone_number
     
