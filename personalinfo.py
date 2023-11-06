cd2f8733ea8b4567da6ee1692acdafe858e367e3
Kalobd12 <143021970+Kalobd12@users.noreply.github.com>
2023-11-06 09:11:11 -0500

    using set method to assign a value to the following data attributes

--- personalinfo.py
+++ personalinfo.py
@@ -1,8 +1,21 @@
 # PersonalData class holds data about personal information.
-class PersonalData
+class PersonalData:
     # The __init__ method initializes the attributes.
     def __init__(self, name, address, age, phone_number):
         self.__name = name
         self.__address = address
         self.__age = age
         self.__phone_number = phone_number
+    # The set_name method accepts an argument for the persons name.     
+    def set_name(self, name):
+        self.__name = name
+    # The set_address method accepts an argument for the persons address.
+    def set_address(self, address):
+        self.__address = address
+    # The set_age method accepts an argument for the persons age.
+    def set_age(self, address):
+        self.__age = age
+    # The set_phone_number method accepts an argument for the persons phone number.
+    def set_phone_number(self, phone_number)
+        self.__phone_number = phone_number
+    
